b2afadd65b63897feae1a50f74ad147d0ceba5df
Matheus Goncalves <matheus.goncalves@dbccompany.com.br>
2022-10-13 14:11:59 -0300

    Correção homework2

--- modulo-2/homework2.sql
+++ modulo-2/homework2.sql
@@ -2,10 +2,10 @@
 SELECT * FROM VEM_SER.PAIS ORDER BY NOME DESC;
 
 --- Selecionar logradouro e cep dos endereços. Porém, somente os logradouros que comecem com a letra ‘a’ (maiúsculo ou minúsculo);
-SELECT LOGRADOURO, CEP FROM VEM_SER.ENDERECO WHERE LOGRADOURO LIKE 'a%' OR LIKE 'A%';
+SELECT LOGRADOURO, CEP FROM VEM_SER.ENDERECO WHERE UPPER(LOGRADOURO) LIKE 'A%';
 
 --- Selecionar todos os endereços que tenham cep com final ‘0’;
-SELECT * FROM VEM_SER.ENDERECO WHERE CEP LIKE '%0';
+SELECT * FROM VEM_SER.ENDERECO WHERE TRIM(CEP) LIKE '%0';
 
 --- Selecionar todos os endereços que tenham números entre 1 e 100;
 SELECT * FROM VEM_SER.ENDERECO WHERE NUMERO BETWEEN 1 AND 100;
