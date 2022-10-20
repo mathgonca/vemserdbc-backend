556ac4ed242db466dffbd0d4cf7197c0646c3dd7
Matheus Goncalves <matheus.goncalves@dbccompany.com.br>
2022-10-19 21:18:59 -0300

    Commit inicial

---
+++ modulo-3.1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/controller/EnderecoController.java
@@ -0,0 +1,15 @@
+package br.com.dbc.vemser.pessoaapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/endereco")
+public class EnderecoController {
+//    GET “/endereco” : recupera todos os endereços.
+//    GET “/endereco/{idEndereco}”: recupera o endereço específico.
+//    GET “/endereco/{idPessoa}/pessoa”: recupera os endereços por pessoa.
+//    POST “/endereco/{idPessoa}”: recebe a pessoa, o endereço e cria o endereço com id da pessoa.
+//    PUT “/endereco/{idEndereco}”: altera os dados do endereço.
+//    DELETE “/endereco/{idEndereco}”: remove o endereço pelo id.
+}
