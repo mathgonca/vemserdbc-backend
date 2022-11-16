8221d7117548ce27e373332879c18e34f861ac4b
Matheus Goncalves <math3usgoncalves@protonmail.com>
2022-11-16 19:43:41 -0300

    Homework 4.2 métodos Find

---
+++ modulo-4.1/MongoJava/src/main/java/br/com/dbc/vemser/find/Main.java
@@ -0,0 +1,39 @@
+package br.com.dbc.vemser.find;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+
+public class Main {
+    public static void main(String[] args) {
+        String uri = "mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+        MongoClient mongoClient = MongoClients.create(uri);
+
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("vemserdbc");
+
+        MongoCollection<Document> alunos = mongoDatabase.getCollection("alunos");
+
+        System.out.println("-- Aluno");
+        System.out.println("-".repeat(75));
+        System.out.println("-- Find Queries");
+        System.out.println("-".repeat(75));
+        System.out.println("-- Find Todos");
+        alunos.find().forEach(System.out::println);
+
+        Document aluno = alunos.find(new Document("nome", "Gabriel"))
+                .first();
+        System.out.println("-".repeat(75));
+        System.out.println("-- Find Gabriel");
+        System.out.println(aluno);
+
+        System.out.println("-".repeat(75));
+        System.out.println("-- Find alunos com Idade menor ou igual a 25");
+        alunos.find(Filters.lte("idade", 25))
+                .forEach(System.out::println);
+
+        mongoClient.close();
+    }
+}
