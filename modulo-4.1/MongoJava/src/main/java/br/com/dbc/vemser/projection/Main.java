3d29296d1d16a70f5c553f17679638c33b01cfe4
Matheus Goncalves <math3usgoncalves@protonmail.com>
2022-11-16 23:10:43 -0300

    Homework 4.2 métodos Projection

---
+++ modulo-4.1/MongoJava/src/main/java/br/com/dbc/vemser/projection/Main.java
@@ -0,0 +1,45 @@
+package br.com.dbc.vemser.projection;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+
+import static com.mongodb.client.model.Projections.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String uri = "mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+        MongoClient mongoClient = MongoClients.create(uri);
+
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("vemserdbc");
+
+        MongoCollection<Document> alunos = mongoDatabase.getCollection("alunos");
+
+        System.out.println("-- Aluno");
+        System.out.println("-".repeat(75));
+        System.out.println("-- Find Queries");
+        System.out.println("-".repeat(75));
+        System.out.println("-- Find Todos");
+        alunos.find()
+                .projection(fields(include("nome", "data_nascimento"), excludeId()))
+                .forEach(document -> System.out.println(document.toJson()));
+
+        Document aluno = alunos.find(new Document("nome", "Gabriel"))
+                .projection(fields(include("idade", "curso.nome", "status")))
+                .first();
+        System.out.println("-".repeat(75));
+        System.out.println("-- Find Gabriel");
+        System.out.println(aluno.toJson());
+
+        System.out.println("-".repeat(75));
+        System.out.println("-- Find alunos com Idade menor ou igual a 25");
+        alunos.find(Filters.lte("idade", 25))
+                .projection(fields(include("idade", "habilidades", "notas"), excludeId()))
+                .forEach(document -> System.out.println(document.toJson()));
+
+        mongoClient.close();
+    }
+}
